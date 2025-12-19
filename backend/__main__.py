de99ccc2f74ce8a52616cdf7af7c098f7c3bc80b
Robotz213 <nicholas@robotz.dev>
2025-12-19 02:36:33 -0300

    🔧 refact: remove handler de log     Remove a classe CustomStreamHandler que não era utilizada, simplificando o código.
    Arquivos Alterados:
        - backend/__main__.py: remove handler

--- backend/__main__.py
+++ backend/__main__.py
@@ -11,15 +11,6 @@ _defaultFormatter = logging.Formatter()  # noqa: N816
 type AnyType = any
 
 
-class CustomStreamHandler(logging.StreamHandler):  # noqa: D101
-    def format(self, record: logging.LogRecord) -> str:
-        return _defaultFormatter.format(record)
-
-    def emit(self, record: logging.LogRecord) -> None:
-        msg = self.format(record)
-        print(name_record=record.name, *(msg,))  # noqa: B026, T201
-
-
 if __name__ == "__main__":
     _start_backend()
 
