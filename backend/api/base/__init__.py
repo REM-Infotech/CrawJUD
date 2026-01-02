8998cb9f2834689cc2eb8c2635f8c84028f8d9cc
Nicholas Silva <nicholasazs213@gmail.com>
2026-01-02 10:24:05 -0400

    🔧 refatoração: Remoção de arquivos     Remoção de arquivos não utilizados para limpeza do código.
    Arquivos Alterados:
        - backend/api/base/__init__.py: arquivo removido
        - backend/api/base/_socketio.py: arquivo removido
        - backend/api/base/_sqlalchemy/__init__.py: arquivo removido
        - backend/api/base/_sqlalchemy/_model.py: arquivo removido
        - backend/api/base/_sqlalchemy/_query.py: arquivo removido
        - backend/api/base/_tst.py: arquivo removido

--- backend/api/base/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Bases customizadas para classes de Extensões e Modelos."""
-
-from backend.base.sqlalchemy._model import Model
-from backend.base.sqlalchemy._query import Query
-
-from ._socketio import BlueprintNamespace
-
-__all__ = ["BlueprintNamespace", "Model", "Query"]
